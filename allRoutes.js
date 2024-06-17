c4e92ab5dd8a3afdda7b18ac1258de582b615a23
Lucile Trichet <luciletrc@gmail.com>
2024-06-17 14:30:06 +0200

    ajout authentification

--- allRoutes.js
+++ allRoutes.js
@@ -2,7 +2,13 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/catalogue", "Catalogue", "/pages/catalogue.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Deconnexion", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Gamestore";
