2985eebee6ac5c728c3dd5af29c1d12b4f41f445
Lucile Trichet <luciletrc@gmail.com>
2024-06-18 19:00:09 +0200

    création de compte

--- allRoutes.js
+++ allRoutes.js
@@ -5,9 +5,11 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/catalogue", "Catalogue", "/pages/catalogue.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Deconnexion", "/pages/auth/signup.html"),
+    new Route("/signup", "Deconnexion", "/pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/orders", "Historique des commandes", "/pages/orders/allOrders.html"),
+    new Route("/cart", "Mon panier", "/pages/orders/cart.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
